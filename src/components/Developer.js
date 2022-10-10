9173d5e70425b3834f7290b77621826a20511e9c
Alhaimouni <m.alhaumouni@gmail.com>
2022-10-10 18:26:16 +0300

    adding some works

---
+++ src/components/Developer.js
@@ -0,0 +1,40 @@
+
+const initialState = [];
+
+
+function Developer(state = initialState, action) {
+  let { type, payload } = action;
+  switch (type) {
+    case 'ADD_DEVELOPER': {
+      const newDev = {
+        id: payload.id,
+        name: payload.name,
+      }
+      const newState = [...state, newDev];
+      return newState;
+    }
+    case 'REMOVE_DEVELOPER': {
+      const newState = state.filter(user => user.id !== payload);
+      return newState;
+    }
+    default:
+      return state;
+  }
+}
+
+function addAction(userData) {
+  return {
+    type: 'ADD_DEVELOPER',
+    payload: userData,
+  }
+}
+
+function removeAction(id) {
+  return {
+    type: 'REMOVE_DEVELOPER',
+    payload: id,
+  }
+}
+
+
+export { Developer, addAction, removeAction };
