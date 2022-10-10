933c9ea38b35f167007c038f3b9dca9206874f67
Alhaimouni <m.alhaumouni@gmail.com>
2022-10-10 21:15:56 +0300

    add styles

--- src/components/DeveloperList.js
+++ src/components/DeveloperList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { removeAction } from './Developer';
 import User from './User';
+import '../App.css';
 
 function DeveloperList({ users, dispatch }) {
   return (
-    <div>
+    <div className='users'>
         {(users.length) ? users.map((user, index) => <User key={index} user={user} dispatch={dispatch} />) : <p>No users</p>}
     </div>
   )
